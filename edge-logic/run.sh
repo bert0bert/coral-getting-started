8e3e4c1d0c129dba340c66ccd7e504ebb2def801
bert0bert <albertonajera2006@gmali.com>
2023-08-04 10:34:11 -0400

    models. updated

--- edge-logic/run.sh
+++ edge-logic/run.sh
@@ -4,9 +4,13 @@
 # If the model or labels are not available, the container 
 # will exit and try again.
 python3 src/classify_image.py \
---model /models/model.tflite \
+--model /models/amodel.tflite \
 --labels /models/labels.txt \
---input images/parrot.jpg
+--input images/image.jpg
+python3 src/classify_image.py \
+--model /models/amodel_edgetpu.tflite \
+--labels /models/labels.txt \
+--input images/image.jpg
 
 # Run balena-idle after our py3 script exits so the container keeps running
 balena-idle
